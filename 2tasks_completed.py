83dbbc066bf5bc4f4fb700cbac64fcac706c77ae
Torben <59419684+entorb@users.noreply.github.com>
2024-02-14 23:16:25 +0100

    refactoring 2: tasks completed and tasks overdue

---
+++ 2tasks_completed.py
@@ -0,0 +1,42 @@
+"""
+RTM tasks completed this year.
+
+by Dr. Torben Menke https://entorb.net
+"""
+
+import datetime as dt
+
+from helper import (
+    DATE_TODAY,
+    df_name_url_to_html,
+    df_to_html,
+    get_lists_dict,
+    get_tasks_as_df,
+)
+
+if __name__ == "__main__":
+    lists_dict = get_lists_dict()
+
+    print("# RTM tasks completed this year")
+    my_date = dt.date(DATE_TODAY.year, 1, 1)
+    df = get_tasks_as_df(
+        my_filter=f'CompletedAfter:{my_date.strftime("%d/%m/%Y")}',
+        lists_dict=lists_dict,
+    )
+    df = df.sort_values(
+        by=["completed", "prio", "name"], ascending=[False, False, True]
+    )
+    df = df.reset_index()
+
+    df = df_name_url_to_html(df)
+    df = df.drop(columns=["list_id", "task_id", "url"])
+    df_to_html(df, "out-completed.html")
+    # df.to_excel("out-done-year.xlsx", index=False)
+
+    df = df.groupby(["completed_week"]).agg(
+        count=("completed_week", "count"),
+        sum_prio=("prio", "sum"),
+        sum_overdue_prio=("overdue_prio", "sum"),
+        sum_estimate=("estimate", "sum"),
+    )
+    print(df)
---
+++ 3tasks_overdue.py
@@ -0,0 +1,36 @@
+"""
+RTM tasks overdue.
+
+by Dr. Torben Menke https://entorb.net
+"""
+
+from helper import (
+    df_name_url_to_html,
+    df_to_html,
+    get_lists_dict,
+    get_tasks_as_df,
+)
+
+if __name__ == "__main__":
+    lists_dict = get_lists_dict()
+
+    print("# RTM tasks overdue")
+    my_filter = """
+dueBefore:Today
+AND NOT status:completed
+AND NOT list:Taschengeld
+"""
+    df = get_tasks_as_df(
+        my_filter=my_filter,
+        lists_dict=lists_dict,
+    )
+    df = df.sort_values(by=["overdue_prio"], ascending=False)
+    df = df.reset_index()
+
+    print(df[["name", "due", "overdue", "prio", "overdue_prio", "estimate"]])
+
+    df = df_name_url_to_html(df)
+    df_to_html(
+        df[["name", "due", "overdue", "prio", "overdue_prio", "estimate"]],
+        "out-overdue.html",
+    )

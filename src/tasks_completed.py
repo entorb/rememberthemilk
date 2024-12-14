130fed373f606ce303b1f696f15d8460da3aa0e8
Torben <59419684+entorb@users.noreply.github.com>
2024-12-14 07:28:42 +0100

    Added Streamlit

--- src/tasks_completed.py
+++ src/tasks_completed.py
@@ -1,3 +1,6 @@
+# by Dr. Torben Menke https://entorb.net
+# https://github.com/entorb/rememberthemilk
+
 """
 RTM tasks completed this year.
 
@@ -5,6 +8,8 @@ by Dr. Torben Menke https://entorb.net
 """
 
 import datetime as dt
+
+from pandas import DataFrame
 
 from helper import (
     DATE_TODAY,
@@ -20,11 +25,8 @@ CompletedAfter:{DATE_START.strftime("%d/%m/%Y")}
 AND NOT list:Taschengeld"""
 
 
-if __name__ == "__main__":
+def get_tasks_completed() -> DataFrame:  # noqa: D103
     lists_dict = get_lists_dict()
-
-    print("# RTM tasks completed this year")
-
     df = get_tasks_as_df(
         my_filter=FILTER_COMPLETED,
         lists_dict=lists_dict,
@@ -34,7 +36,6 @@ if __name__ == "__main__":
     )
     df = df.reset_index()
 
-    df = df_name_url_to_html(df)
     cols = [
         "name",
         "list",
@@ -46,20 +47,41 @@ if __name__ == "__main__":
         "overdue_prio",
         "postponed",
         "estimate",
+        "url",
     ]
+    df = df[cols]
+
+    return df
+
+
+def completed_week(df: DataFrame) -> DataFrame:  # noqa: D103
+    df = (
+        df.groupby(["completed_week", "list"])
+        .agg(
+            count=("completed_week", "count"),
+            sum_prio=("prio", "sum"),
+            sum_overdue_prio=("overdue_prio", "sum"),
+            sum_estimate=("estimate", "sum"),
+        )
+        .sort_values(by=["completed_week", "list"], ascending=[False, True])
+    )
+    return df
+
+
+if __name__ == "__main__":
+    df = get_tasks_completed()
+
+    print("# RTM tasks completed this year")
+
+    df = df_name_url_to_html(df)
 
     df_to_html(
-        df[cols],
+        df,
         "out-completed.html",
     )
     # df.to_excel(output_dir/"out-done-year.xlsx", index=False)
 
-    df2 = df.groupby(["completed_week", "list"]).agg(
-        count=("completed_week", "count"),
-        sum_prio=("prio", "sum"),
-        sum_overdue_prio=("overdue_prio", "sum"),
-        sum_estimate=("estimate", "sum"),
-    )
+    df2 = completed_week(df)
     print(df2)
 
     df_to_html(df2, "out-completed-week.html", index=True)

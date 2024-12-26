f423ad7cc089036e28a1c6e85d0a78a6b5dbc433
Torben <59419684+entorb@users.noreply.github.com>
2024-12-26 23:48:16 +0100

    proper multipage app

--- src/app.py
+++ src/app.py
@@ -1,10 +1,25 @@
 """Streamlit UI."""
 
+from pathlib import Path
+
 import streamlit as st
 
-from helper import delete_cache
+# from helper import delete_cache
 
 st.set_page_config(page_title="RTM Report", page_icon=None, layout="wide")
-st.title("RTM")
 
-delete_cache()
+
+def create_navigation_menu() -> None:
+    """Create and populate navigation menu."""
+    lst = []
+    for p in sorted(Path("src/reports").glob("*.py")):
+        f = p.stem
+        t = f[3:]
+        lst.append(st.Page(page=f"reports/{f}.py", title=t))
+    pg = st.navigation(lst)
+    pg.run()
+
+
+create_navigation_menu()
+
+# delete_cache()
